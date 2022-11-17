1cc5a4217920565603b42fa8e5fe76257e6cfe9a
Shuhrat <shuhratnematjonov1991@gmail.com>
2022-11-17 20:19:17 +0500

    added

--- lesson_20.py
+++ lesson_20.py
@@ -1,4 +1,39 @@
-"""git lesson"""
+"""git lesson
+
+System: git config --system : настройки на системном уровне
+Use: git config --global : настройки на глобалном  уровне
+Project: git config : настройки на уровне проекта
+ 
+git config --global user.name "name in github" 
+git config --global user.email "realemail" 
+ 
+git init : создание локального пустого репозитория .git файлом 
+
+git config --list : виводит все настройки конфигурации 
+git config --global core.editor : 
+ 
+git status - Чтобы узнать нынешний статус проекта 
+git branch -M main 
+
+git log :
+
+git add . 
+git commit -m "message" 
+git push 
+ 
+git add название_файла 
+git commit  
+git push 
+ 
+git remote add origin ссылка_на_репозиторий 
+git push --set-upstream origin main : для создания ветки на глобалном уровне
+
+git checkout -b name branch(vetka) для создания ветки на главный ветке main 
+
+
+
+
+"""
 
 
 
